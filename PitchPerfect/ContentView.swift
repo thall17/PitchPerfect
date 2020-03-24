0513e254419561f1837ef90a4be33d958274ec44
Tim Hall <thall74@gatech.edu>
2020-03-23 21:25:23 -0700

    First commit

---
+++ PitchPerfect/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PitchPerfect
+//
+//  Created by Tim on 3/23/20.
+//  Copyright © 2020 timhall. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
