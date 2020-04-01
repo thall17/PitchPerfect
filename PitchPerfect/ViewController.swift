dc40ba29783f3d2df41a64fb9bf89d530a562a99
Tim Hall <timhall@apple.com>
2020-03-31 22:41:27 -0700

    Added assets. Need to fix AutoLayout

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,18 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        stopRecordingButton.isEnabled = false
     }
 
     @IBAction func recordAudio(_ sender: Any) {
+        recordButton.isEnabled = false
+        stopRecordingButton.isEnabled = true
         recordingLabel.text = "Recording in progress"
     }
     
     @IBAction func stopRecording(_ sender: Any) {
-        recordingLabel.text = "Tap to Record"
+        stopRecordingButton.isEnabled = false
+        recordButton.isEnabled = true
+        recordingLabel.text = "Tap to record"
     }
 }
 
